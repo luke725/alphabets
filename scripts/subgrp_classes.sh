b3d5ba716650a830c2fb9b30f8801230f59bb14a
Lukasz Wolochowski <l.wolochowski@students.mimuw.edu.pl>
2014-10-29 13:34:42 +0100

    cleanup scripts

--- scripts/subgrp_classes.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "List(List(ConjugacyClassesSubgroups(SymmetricGroup($1)), Elements), l->List(l,GeneratorsOfGroup));" | gap -q | sed s/\)\(/],[/g | sed -e s/\(/[[/g -e s/\)/]]/g
-
--- scripts/subgrps.sh
+++ scripts/subgrps.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# author: Lukasz Wolochowski (l.wolochowski@students.mimuw.edu.pl)
+# a script for generating all representatives of conjugacy classes of S_n, where n is $1
+# output of the script can be used as an input of [alphabets all]
+
 echo "List(List(ConjugacyClassesSubgroups(SymmetricGroup($1)), Representative), GeneratorsOfGroup);" | gap -q | sed s/\)\(/],[/g | sed -e s/\(/[[/g -e s/\)/]]/g
 
